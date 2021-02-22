a800b3f46290aee996015bec29a1cc84227df3ba
Antoine Leon <thelifeofantoine@outlook.com>
2021-02-22 14:31:32 -0800

    moved .ui templates into /UI/templates/

--- UI/builder_widget.py
+++ UI/builder_widget.py
@@ -96,5 +96,5 @@ class SalientArtifactWindow(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = BuilderWindow()
+    window = BuilderWidget()
     sys.exit(app.exec())
