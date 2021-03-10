2ff6eb5afbc8b06b8a71c280216457c12b087875
valmaci <valmacias5@gmail.com>
2021-03-10 14:50:18 -0700

    builder menu

--- UI/BuilderWidget.py
+++ UI/BuilderWidget.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtGui import QStandardItem
@@ -22,6 +23,7 @@ class BuilderWidget(QWidget):
     def UI(self):
         self.gridLayout = QGridLayout(self)
         self.gridLayout.setObjectName(u"gridLayout")
+        self.setLayout(self.gridLayout)
 
         #Create search bar
         self.label = QLabel('Builder', self)
@@ -42,12 +44,62 @@ class BuilderWidget(QWidget):
         self.gridLayout.addWidget(self.edit_artifacts_button, 1, 0)
         self.edit_artifacts_button.clicked.connect(self.openArtifacts)
 
+        '''
+        # Save Button
         self.save_button = QPushButton('Save Project', self)
         self.gridLayout.addWidget(self.save_button, 3, 2)
         self.save_button.clicked.connect(self.save_script)
+        '''
 
-        self.setLayout(self.gridLayout)
-        
+        # Setup Menu Bar
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0,0,482,21))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuFile.setTitle("File")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        self.menuEdit.setTitle("Edit")
+        self.gridLayout.addWidget(self.menubar, 0,0)
+
+        # Setup Menu Actions
+        self.actionOpen_Script = QtWidgets.QAction(self, triggered=self.open_script)
+        self.actionOpen_Script.setObjectName("actionOpen_Script")
+        self.actionOpen_Script.setText("Open Script")
+        self.menuFile.addAction(self.actionOpen_Script)
+        self.actionSave_Script = QtWidgets.QAction(self)
+        self.actionSave_Script.setObjectName("actionSave_Script")
+        self.actionSave_Script.setText("Save Script")
+        self.menuFile.addAction(self.actionSave_Script)
+        self.actionSave_Script_As = QtWidgets.QAction(self, triggered=self.save_script)
+        self.actionSave_Script_As.setObjectName("actionSave_Script_As")
+        self.actionSave_Script_As.setText("Save Script As")
+        self.menuFile.addAction(self.actionSave_Script_As)
+        self.actionUndo = QtWidgets.QAction(self)
+        self.actionUndo.setObjectName("actionUndo")
+        self.actionUndo.setText("Undo") 
+        self.menuEdit.addAction(self.actionUndo)
+        self.actionRedo = QtWidgets.QAction(self)
+        self.actionRedo.setObjectName("actionRedo")
+        self.actionRedo.setText("Redo") 
+        self.menuEdit.addAction(self.actionRedo)
+        self.actionCopy = QtWidgets.QAction(self)
+        self.actionCopy.setObjectName("actionCopy")
+        self.actionCopy.setText("Copy") 
+        self.menuEdit.addAction(self.actionCopy)
+        self.actionPaste = QtWidgets.QAction(self)
+        self.actionPaste.setObjectName("actionPaste")
+        self.actionPaste.setText("Paste") 
+        self.menuEdit.addAction(self.actionPaste)
+        self.actionCut = QtWidgets.QAction(self)
+        self.actionCut.setObjectName("actionCut")
+        self.actionCut.setText("Cut") 
+        self.menuEdit.addAction(self.actionCut)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+
     def openArtifacts(self):
         if ProjectController.is_project_loaded():
             self.artifacts_window.populate_table()
@@ -65,6 +117,16 @@ class BuilderWidget(QWidget):
                 return False 
         #TODO: Write file into path (need to figure out format)
     
+    def open_script(self):
+        if not self.script_file_path:
+            new_file_path, filter_type = QFileDialog.getOpenFileName(self, "Save this script as...", "", "All files(*)")
+            if new_file_path:
+                self.script_file_path = new_file_path
+            else:
+                self.invalid_path_alert_message()
+                return False 
+        #TODO: What to do with file (read it and write it in appropriate place)
+
     def invalid_path_alert_message(self):
         messageBox = QMessageBox()
         messageBox.setWindowTitle("Invalid file")
@@ -72,6 +134,6 @@ class BuilderWidget(QWidget):
         messageBox.exec()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = BuilderWidget()
     sys.exit(app.exec())
