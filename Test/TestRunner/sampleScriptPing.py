091f0ec7b8e737b5bfce1898ee54c3fdbda5c6ec
valmaci <valmacias5@gmail.com>
2021-03-26 11:51:24 -0600

    run confirm

--- Test/TestRunner/sampleScriptPing.py
+++ Test/TestRunner/sampleScriptPing.py
@@ -2,6 +2,7 @@ import pyautogui
 from PIL import Image
 import os, os.path
 
+print("SCRIPT IS RUNNING\n")
 pyautogui.PAUSE = 1.5
 x, y = pyautogui.locateCenterOnScreen(os.path.join('/home/kali/Desktop','terminalScreenshotRedDot.png'), confidence=0.5)
 pyautogui.click(x,y)
