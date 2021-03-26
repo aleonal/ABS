fe6052ec8a1c4a28fc4e08254b050f657b726e35
Diego A. Rivera <darivera2@miners.utep.edu>
2021-03-26 07:05:19 -0600

    Added window to review click settings and rearranged the interface.

--- UI/BuilderWidget.py
+++ UI/BuilderWidget.py
@@ -9,6 +9,7 @@ import json
 from src.ProjectController import ProjectController
 from src import Event
 from UI.ArtifactsTableWidget import SalientArtifactWindow
+from UI.ClickOptionsWidget import ClickOptionsWidget
 
 class BuilderWidget(QWidget):
 
@@ -31,23 +32,73 @@ class BuilderWidget(QWidget):
         self.label = QLabel('Builder', self)
         self.label.setObjectName(u"label")
         self.label.setFont(QFont('MS Shell Dlg 2', 12))
-        self.gridLayout.addWidget(self.label, 1, 1)
+        self.gridLayout.addWidget(self.label, 0, 0)
+        
+        self.horizontalTopLeftLayoutWidget = QWidget(self)
+        self.horizontalTopLeftLayoutWidget.setObjectName(u"horizontalTopLeftLayoutWidget")
+        self.horizontalTopLeftLayout = QHBoxLayout(self.horizontalTopLeftLayoutWidget)
+        self.horizontalTopLeftLayout.setObjectName(u"horizontalTopLayout")
+        self.gridLayout.addWidget(self.horizontalTopLeftLayoutWidget, 1, 0)
 
-        self.search_button = QPushButton('Search', self)
-        self.gridLayout.addWidget(self.search_button, 1,3)
-        self.search_button.setEnabled(False)
+        self.search_relationships_lineedit = QLineEdit(self)
+        self.horizontalTopLeftLayout.addWidget(self.search_relationships_lineedit, 0)
+        self.relationship_search_button = QPushButton('Search', self)
+        self.horizontalTopLeftLayout.addWidget(self.relationship_search_button, 1)
+        self.relationship_search_button.setEnabled(False)
+        
+        self.horizontalTopRightLayoutWidget = QWidget(self)
+        self.horizontalTopRightLayoutWidget.setObjectName(u"horizontalTopRightLayoutWidget")
+        self.horizontalTopRightLayout = QHBoxLayout(self.horizontalTopRightLayoutWidget)
+        self.horizontalTopRightLayout.setObjectName(u"horizontalTopRightLayout")
+        self.gridLayout.addWidget(self.horizontalTopRightLayoutWidget, 1, 3)
 
-        self.lineEdit = QLineEdit(self)
-        self.gridLayout.addWidget(self.lineEdit, 1,2)
+        self.search_dependency_lineedit = QLineEdit(self)
+        self.horizontalTopRightLayout.addWidget(self.search_dependency_lineedit, 0)
+        self.dependency_search_button = QPushButton('Search', self)
+        self.horizontalTopRightLayout.addWidget(self.dependency_search_button, 1)
+        self.dependency_search_button.setEnabled(False)
 
-        self.listrelationships = ABSEventTreeWidget()
+
+        self.listrelationships = ABSRelationshipTreeWidget()
         self.gridLayout.addWidget(self.listrelationships, 2, 0)
 
-        self.listdependencies = ABSEventTreeWidget()
+        self.verticalCenterLayoutWidget = QWidget(self)
+        self.verticalCenterLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalCenterLayout = QVBoxLayout(self.verticalCenterLayoutWidget)
+        self.verticalCenterLayout.setObjectName(u"verticalLayout")
+        self.gridLayout.addWidget(self.verticalCenterLayoutWidget, 2, 1)
+
+        self.move_node_button = QPushButton('>', self)
+        self.verticalCenterLayout.addWidget(self.move_node_button)
+        self.move_node_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
+        #self.move_node_button.clicked.connect(self.openArtifacts)
+        self.move_node_button.setEnabled(False)
+
+        self.move_tree_button = QPushButton('>>', self)
+        self.verticalCenterLayout.addWidget(self.move_tree_button)
+        self.move_tree_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
+        #self.move_tree_button.clicked.connect(self.openArtifacts)
+        self.move_tree_button.setEnabled(False)
+
+        self.click_button = QPushButton('Click Settings', self)
+        self.verticalCenterLayout.addWidget(self.click_button)
+        self.click_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
+        self.click_button.clicked.connect(self.openClicks)
+        self.click_button.setEnabled(False)
+
+        self.keypress_button = QPushButton('Keypress Settings', self)
+        self.verticalCenterLayout.addWidget(self.keypress_button)
+        self.keypress_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
+        #self.move_tree_button.clicked.connect(self.openArtifacts)
+        self.keypress_button.setEnabled(False)
+
+        self.verticalCenterLayout.setAlignment(Qt.AlignCenter)
+
+        self.listdependencies = ABSDependencyTreeWidget()
         self.gridLayout.addWidget(self.listdependencies, 2, 2, 1, 2)
 
         self.edit_artifacts_button = QPushButton('Edit Salient Artifacts', self)
-        self.gridLayout.addWidget(self.edit_artifacts_button, 1, 0)
+        self.gridLayout.addWidget(self.edit_artifacts_button, 3, 0)
         #self.edit_artifacts_button.setFixedSize(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         self.edit_artifacts_button.setSizePolicy(QtWidgets.QSizePolicy.Fixed,QtWidgets.QSizePolicy.Fixed)
         self.edit_artifacts_button.clicked.connect(self.openArtifacts)
@@ -66,7 +117,10 @@ class BuilderWidget(QWidget):
             self.populateTrees()
             self.save_button.setEnabled(True)
             self.edit_artifacts_button.setEnabled(True)
-            self.search_button.setEnabled(True)
+            self.relationship_search_button.setEnabled(True)
+            self.dependency_search_button.setEnabled(True)
+            self.move_tree_button.setEnabled(True)
+            self.click_button.setEnabled(True)
 
     def populateTrees(self):
         eventGroups = ProjectController.load_event_list()
@@ -117,6 +171,16 @@ class BuilderWidget(QWidget):
         else:
             QMessageBox.critical(self, "Project Error", "No project is currently loaded.")
 
+    def openClicks(self):
+        selectedRow = self.listdependencies.selectionModel().selectedRows(0)[0]
+        selectedItem = self.listdependencies.itemAt(selectedRow.row(), selectedRow.column())
+        print (selectedItem.text(0))
+        if selectedItem.text(0) == "clicks_id":
+            self.clicks_window = ClickOptionsWidget(selectedItem.text(2))
+            self.clicks_window.show()
+        else:
+            QMessageBox.critical(self, "Input Error", "Event is not a clicks_id type")
+
     def save_script(self):
         if not self.script_file_path:
             new_file_path, filter_type = QFileDialog.getSaveFileName(self, "Save this script as...", "", "All files(*)")
@@ -133,7 +197,14 @@ class BuilderWidget(QWidget):
         messageBox.setText("Selected filename or path is not valid. Please select a valid file.")
         messageBox.exec()
 
-class ABSEventTreeWidget(QTreeWidget):
+    def enable_clicks_button(self):
+        self.click_button.setEnabled(True)
+    
+    # def dependency_event_selection_changed(selectedItems, deselectedItems):
+    #     if selectedItems[0][0] == "clicks_id":
+    #         self.enable_clicks_button(True)
+
+class ABSRelationshipTreeWidget(QTreeWidget):
     def __init__(self):
         super().__init__()
         ___qtreewidgetitem = self.headerItem()
@@ -163,56 +234,88 @@ class ABSEventTreeWidget(QTreeWidget):
             tempQtreewidgetitem.setText(0,_type)
             return tempQtreewidgetitem
     
-    def dropEvent(self, event):
-        md = event.mimeData()
-        fmt = "application/x-qabstractitemmodeldatalist"
-        dropIndex = self.indexAt(event.pos())
-        dropIndicator = QAbstractItemView.dropIndicatorPosition(self)
+    # def dropEvent(self, event):
+    #     md = event.mimeData()
+    #     fmt = "application/x-qabstractitemmodeldatalist"
+    #     dropIndex = self.indexAt(event.pos())
+    #     dropIndicator = QAbstractItemView.dropIndicatorPosition(self)
 
-        if md.hasFormat(fmt):
-            encoded = md.data(fmt)
-            stream = QtCore.QDataStream(encoded, QtCore.QIODevice.ReadOnly)
-            tree_items = []
-            parent = self
+    #     if md.hasFormat(fmt):
+    #         encoded = md.data(fmt)
+    #         stream = QtCore.QDataStream(encoded, QtCore.QIODevice.ReadOnly)
+    #         tree_items = []
+    #         parent = self
 
-            while not stream.atEnd():
-                it = QtWidgets.QTreeWidgetItem()
-                # row and column where it comes from
-                for j in range(3): # 3 columns in the tree
-                    row = stream.readInt32()
-                    column = stream.readInt32()
-                    map_items = stream.readInt32()
+    #         while not stream.atEnd():
+    #             it = QtWidgets.QTreeWidgetItem()
+    #             # row and column where it comes from
+    #             for j in range(3): # 3 columns in the tree
+    #                 row = stream.readInt32()
+    #                 column = stream.readInt32()
+    #                 map_items = stream.readInt32()
 
-                    for i in range(map_items):
-                        role = stream.readInt32()
-                        value = QtCore.QVariant()
-                        stream >> value
-                        it.setData(column, role, value)
-                tree_items.append(it)
+    #                 for i in range(map_items):
+    #                     role = stream.readInt32()
+    #                     value = QtCore.QVariant()
+    #                     stream >> value
+    #                     it.setData(column, role, value)
+    #             tree_items.append(it)
 
-            if (not dropIndex.parent().isValid() and dropIndex.row() != -1):
-                if dropIndicator == QAbstractItemView.AboveItem:
-                    # manage a boolean for the case when you are above an item
-                    for it in tree_items:
-                        parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
-                    return
-                elif dropIndicator == QAbstractItemView.BelowItem:
-                    #something when being below an item
-                    for it in tree_items:
-                        parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
-                    return
-                elif dropIndicator == QAbstractItemView.OnItem:
-                    #you're on an item, maybe add the current one as a child
-                    parent = self.itemAt(dropIndex.row(), dropIndex.column())
-                    for it in tree_items:
-                        parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
-                    return
-                elif dropIndicator == QAbstractItemView.OnViewport:
-                    #you are not on your tree
-                    return
+    #         if (not dropIndex.parent().isValid() and dropIndex.row() != -1):
+    #             if dropIndicator == QAbstractItemView.AboveItem:
+    #                 # manage a boolean for the case when you are above an item
+    #                 for it in tree_items:
+    #                     parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
+    #                 return
+    #             elif dropIndicator == QAbstractItemView.BelowItem:
+    #                 #something when being below an item
+    #                 for it in tree_items:
+    #                     parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
+    #                 return
+    #             elif dropIndicator == QAbstractItemView.OnItem:
+    #                 #you're on an item, maybe add the current one as a child
+    #                 parent = self.itemAt(dropIndex.row(), dropIndex.column())
+    #                 for it in tree_items:
+    #                     parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
+    #                 return
+    #             elif dropIndicator == QAbstractItemView.OnViewport:
+    #                 #you are not on your tree
+    #                 return
             
-            for it in tree_items:
-                        parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
+    #         for it in tree_items:
+    #                     parent = self.addNode(it.text(0), it.text(1), it.text(2), parent, True)
+
+class ABSDependencyTreeWidget(QTreeWidget):
+    def __init__(self):
+        super().__init__()
+        ___qtreewidgetitem = self.headerItem()
+        ___qtreewidgetitem.setText(4,"Match Content")
+        ___qtreewidgetitem.setText(3,"Custom Attributes")
+        ___qtreewidgetitem.setText(2,"Time")
+        ___qtreewidgetitem.setText(1,"Event Subtype")
+        ___qtreewidgetitem.setText(0,"Event Type")
+        self.setAcceptDrops(True)
+        self.setTabKeyNavigation(True)
+        self.setDragEnabled(True)
+        self.setDragDropOverwriteMode(False)
+        self.setDragDropMode(QAbstractItemView.DragDrop)
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.setDefaultDropAction(Qt.CopyAction)
+        self.setSortingEnabled(True)
+        self.setWordWrap(True)
+        self.setSortingEnabled(True)
+
+    def addNode(self, _type=None, time=None, content=None, parent=None, canEdit=False):
+        if(parent is not None):
+            tempQtreewidgetitem = QTreeWidgetItem(parent)
+            if canEdit:
+                tempQtreewidgetitem.setFlags(Qt.ItemIsEditable|Qt.ItemIsSelectable|Qt.ItemIsDragEnabled|Qt.ItemIsDropEnabled|Qt.ItemIsUserCheckable|Qt.ItemIsEnabled)
+            else:
+                tempQtreewidgetitem.setFlags(Qt.ItemIsSelectable|Qt.ItemIsDragEnabled|Qt.ItemIsDropEnabled|Qt.ItemIsUserCheckable|Qt.ItemIsEnabled)
+            tempQtreewidgetitem.setText(4,content)
+            tempQtreewidgetitem.setText(2,time)
+            tempQtreewidgetitem.setText(0,_type)
+            return tempQtreewidgetitem
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
---
+++ UI/ClickOptionsWidget.py
@@ -0,0 +1,136 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtGui import QStandardItem
+from PyQt5.QtCore import *
+
+
+class ClickOptionsWidget(QWidget):
+    def __init__(self, img_file=None):
+        super().__init__()
+        self.setGeometry(50, 50, 838, 633)
+        self.scrollArea = QScrollArea (self)
+        self.scrollAreaWidgetContents = QWidget(self)
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setAutoFillBackground(True)
+        self.verticalLayoutWidget = QWidget(self.scrollAreaWidgetContents)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.imageLabel = QLabel(self.verticalLayoutWidget)
+        self.imageLabel.setObjectName(u"image")
+        self.verticalLayout.addWidget(self.imageLabel,0)
+
+        self.horizontalLayoutWidget = QWidget(self.verticalLayoutWidget)
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setSpacing(20)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.xCoor = QLabel(self.verticalLayoutWidget)
+        self.xCoor.setObjectName(u"xCoor")
+
+        self.horizontalLayout.addWidget(self.xCoor)
+
+        self.xlineEdit = QLineEdit(self.verticalLayoutWidget)
+        self.xlineEdit.setObjectName(u"xlineEdit")
+        self.xlineEdit.setMinimumSize(QSize(10, 20))
+        self.xlineEdit.setMaximumSize(QSize(200, 20))
+        self.xlineEdit.setMaxLength(10)
+
+        self.horizontalLayout.addWidget(self.xlineEdit)
+
+        self.yCoor = QLabel(self.verticalLayoutWidget)
+        self.yCoor.setObjectName(u"yCoor")
+
+        self.horizontalLayout.addWidget(self.yCoor)
+
+        self.ylineEdit = QLineEdit(self.verticalLayoutWidget)
+        self.ylineEdit.setObjectName(u"ylineEdit")
+        self.ylineEdit.setMinimumSize(QSize(10, 20))
+        self.ylineEdit.setMaximumSize(QSize(200, 20))
+        self.ylineEdit.setMaxLength(10)
+
+        self.horizontalLayout.addWidget(self.ylineEdit)
+
+        self.label_2 = QLabel(self.verticalLayoutWidget)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.clickComboBox = QComboBox(self.verticalLayoutWidget)
+        self.clickComboBox.addItem("")
+        self.clickComboBox.addItem("")
+        self.clickComboBox.addItem("")
+        self.clickComboBox.setObjectName(u"clickComboBox")
+        self.clickComboBox.setMinimumSize(QSize(300, 20))
+        self.clickComboBox.setMaximumSize(QSize(400, 20))
+
+        self.horizontalLayout.addWidget(self.clickComboBox)
+
+        self.saveButton = QPushButton(self.verticalLayoutWidget)
+        self.saveButton.setObjectName(u"saveButton")
+        self.saveButton.setMinimumSize(QSize(100, 20))
+        self.saveButton.setMaximumSize(QSize(300, 20))
+
+        self.horizontalLayout.addWidget(self.saveButton)
+
+
+        self.verticalLayout.addWidget(self.horizontalLayoutWidget,1)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.retranslateUi()
+        self.xlineEdit.textChanged[str].connect(self.xCoorChanged)
+        self.ylineEdit.textChanged[str].connect(self.yCoorChanged)
+
+        QMetaObject.connectSlotsByName(self)
+        if img_file is not None:
+            self.loadImage(img_file)
+        #test 
+        else:
+            self.loadImage(r"C:\Users\dgriv\OneDrive\Documents\GitHub\practicum\ECELdSample\Ping\ecel-export_1613199564\raw\pykeylogger\click_images\1613199453.5309389_qterminal_root.png")
+    # setupUi
+
+    def retranslateUi(self):
+        self.setWindowTitle(QCoreApplication.translate("Click Options", u"Click Options", None))
+        self.imageLabel.setText(QCoreApplication.translate("Click Options", u"Screenshot", None))
+        self.xCoor.setText(QCoreApplication.translate("Click Options", u"X Coordinates", None))
+        self.yCoor.setText(QCoreApplication.translate("Click Options", u"Y Coordinates", None))
+        self.label_2.setText(QCoreApplication.translate("Click Options", u"Click Type", None))
+        self.clickComboBox.setItemText(0, QCoreApplication.translate("Click Options", u"leftClick", None))
+        self.clickComboBox.setItemText(1, QCoreApplication.translate("Click Options", u"rightClick", None))
+        self.clickComboBox.setItemText(2, QCoreApplication.translate("Click Options", u"doubleClick", None))
+
+        self.clickComboBox.setCurrentText(QCoreApplication.translate("Click Options", u"leftClick", None))
+        self.saveButton.setText(QCoreApplication.translate("Click Options", u"Save", None))
+    # retranslateUi
+
+    def loadImage(self, img_file=None):
+        if img_file is not None:
+            image = QPixmap(img_file)
+            self.resize(image.width()+100, image.height()+100)
+            self.scrollArea.resize(image.width()+100, image.height()+100)
+            self.scrollAreaWidgetContents.resize(image.width()+100, image.height()+100)
+            self.verticalLayoutWidget.resize(image.width(), image.height() + self.horizontalLayoutWidget.height() + 30)
+            self.horizontalLayoutWidget.setGeometry(0, image.height() + 10, image.width()/2, self.horizontalLayoutWidget.height())
+            self.imageLabel.resize(image.width(), image.height()+20)
+            self.imageLabel.setPixmap(image)
+            self.imageLabel.mousePressEvent = self.getPos
+
+    def getPos(self , event):
+        self.x = event.pos().x()
+        self.y = event.pos().y()
+        self.xlineEdit.setText(str(self.x))
+        self.ylineEdit.setText(str(self.y))
+    
+    def xCoorChanged(self, text):
+        self.xlineEdit.setText(text)
+
+    def yCoorChanged(self, text):
+        self.ylineEdit.setText(text)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = ClickOptionsWidget()
+    window.show()
+    sys.exit(app.exec())
